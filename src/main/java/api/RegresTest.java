a17a2c7970cd114fefed0997b1307a0d7ea25e36
andreybannikov <andrewnnov@yandex.ru>
2022-01-23 13:44:52 +0300

    init rest assured test

---
+++ src/main/java/api/RegresTest.java
@@ -0,0 +1,35 @@
+package api;
+
+import io.restassured.http.ContentType;
+import org.junit.Assert;
+import org.junit.Test;
+import static io.restassured.RestAssured.given;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RegresTest {
+
+    private final static String URL = "https://reqres.in/";
+
+    @Test
+    public void checkAvatarAndIdTest() {
+        List<UserData> users = given()
+                .when()
+                .contentType(ContentType.JSON)
+                .get(URL + "api/users?page=2")
+                .then().log().all()
+                .extract().body().jsonPath().getList("data", UserData.class);
+
+        users.stream().forEach(x -> Assert.assertTrue(x.getAvatar().contains(x.getId().toString())));
+        Assert.assertTrue(users.stream().allMatch(x->x.getEmail().endsWith("@reqres.in")));
+
+        List<String> avatars = users.stream().map(UserData::getAvatar).collect(Collectors.toList());
+        List<String> ids = users.stream().map(x->x.getId().toString()).collect(Collectors.toList());
+
+        for(int i = 0; i < avatars.size(); i++) {
+            Assert.assertTrue(avatars.get(i).contains(ids.get(i)));
+        }
+
+
+    }
+}
